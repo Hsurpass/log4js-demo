741c6eab1b4f248d8e8fdf1df128a3d7b2d3eaf1
Hsurpass <1742143252@qq.com>
2021-02-18 12:48:23 +0800

    update

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,34 @@
 import {logger} from "./logger"
 import { performance } from 'perf_hooks';
 
+logger.trace("trace");
+logger.debug("debug");
+logger.warn("warn");
+logger.info("info");
+logger.error("error");
+logger.fatal("fatal");
+logger.mark("mark");
+
+console.log("---------------------------");
+function createLogProxy (logLevel, logger) {
+    return (...param) => {
+        logger[logLevel](...param);
+    };
+}
+console.log = createLogProxy('debug', logger);
+console.info = createLogProxy('info', logger);
+console.warn = createLogProxy('warn', logger);
+console.error = createLogProxy('error', logger);
+
+console.log("console log");
+console.info("console info");
+console.warn("console warn");
+console.error("console error");
+
+console.log("time:", new Date());
+
 const startTime = performance.now();
-for(let i = 0;i < 100000;i++)
+for(let i = 0;i < 10;i++)
 {
     logger.info("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 }
